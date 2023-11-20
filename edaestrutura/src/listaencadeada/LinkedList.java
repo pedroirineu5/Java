cb70c3b23ec0268299d05eec8fa08178fdc4832f
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2023-11-19 21:53:02 -0300

    fdsmakfmdskal

--- edaestrutura/src/listaencadeada/LinkedList.java
+++ edaestrutura/src/listaencadeada/LinkedList.java
@@ -4,13 +4,26 @@ package listaencadeada;
 
 public class LinkedList<T>{
     private Node<T> start;
+    private Node<T> last;
     private int length = 0;
 
-    
     public void add(T data){
         Node<T> cel = new Node<T>(data);
-        this.start = cel;
-        this.length++;
+        if (this.length == 0){
+            this.start = cel; //A HEAD APONTA PARA O PRIMEIRO NÓ, POR ISSO É UMA HEAD.(Isso também só funciona no primeiro nó, pq depois que só vai rodar é o else)
+        }
+        else{
+            this.last.setNext(cel);// O nó do lado do nó passado no caso ficaria assim NODE=[DATA=1[NEXT=NODE[DATA=2[NEXT=NULL]]]]
+        }        
+        this.last = cel; // atribuindo o TAIL para a ultima estrutura de nó
+        this.length++; //incrementando o size do NÓ para ajudar a contar
+    }
+
+    public void search(T target){
+         
+        while (current.next !=null){
+            atual = current.next
+        }
     }
 
     public int getLength(){
--- edaestrutura/src/listaencadeada/teste/ListaEncadeadaTeste.java
+++ edaestrutura/src/listaencadeada/teste/ListaEncadeadaTeste.java
@@ -5,8 +5,11 @@ import listaencadeada.LinkedList;
 public class ListaEncadeadaTeste {
     public static void main(String[] args){
         LinkedList<Integer> list = new LinkedList<>(); 
-        list.add(1);
 
+        for (int i= 0; i<10;i++){
+            list.add(i);
+
+        }
         System.out.println(list);
         System.out.println("Tamanho" + list.getLength());
 
